69cac6d3ec56247aa40a46df21e7324a76b16200
KsrAser08 <kangsurin08@gmail.com>
2024-05-23 16:50:27 +0900

    0523

--- busanhang(2)/busangagosipda.c
+++ busanhang(2)/busangagosipda.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 //열차 길이
 #define LEN_MIN 15
 #define LEN_MAX 50
@@ -25,33 +24,59 @@
 #define ACTION_REST 0
 #define ACTION_PROVOKE 1
 #define ACTION_PULL 2
-
 //변수 선언
 int train_length; //열차길이
 int percentile_probability; //이동 확률
 int madongseok_stamina; //마동석 체력
 int turn = 0;
+int madongseok, zombie, citizen; //마동석 좀비 시민
+int first_train;
+//함수 선언
+int intro();
+int variables(char massage, int MIN, int MAX);
+int train(int ZOMB, int CITIZ, int MADONGS);
 
-int input_value() {
-
-	
+int intro() {
+	printf(" ------------- busanhang(2) ------------- \n");
+	printf("|                                        |\n");
+	printf("|         We must run the train to       |\n");
+	printf("|              avoid zombies             |\n");
+	printf("|                                        |\n");
+	printf(" ---------------------------------------- \n");
 }
-
+int variables(char*massage,int MIN, int MAX) {
+	int value;
+	while (1) {
+		printf("%s(%d ~ %d)>> ", massage, MIN, MAX);
+		scanf_s("%d", &value);
+		if (value >= MIN && value <= MAX) {
+			break;
+		}
+	}
+	madongseok = train_length - 2;
+	zombie = train_length - 3;
+	citizen = train_length - 6;
+	return value;
+}
+int train(int ZOMB,int CITIZ,int MADONGS) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < train_length; j++) {
+			if (i == 0 || i == 2) printf("#");
+			else {
+				if ((j == 0) || (j == train_length - 1)) printf("#");
+				else if (MADONGS == j) printf("M");
+				else if (CITIZ == j) printf("Z");
+				else if (ZOMB == j) printf("C");
+				else printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
 int main () {
-	//변수 입력받기
-	printf("train length(%d ~ %d)", LEN_MIN, LEN_MAX);
-	scanf_s("%d", &train_length);
-	if (train_length < LEN_MIN || train_length > LEN_MAX) {
-		
-
-	}
-	printf("percentile probability  (%d ~ %d) >> ", PROB_MIN, PROB_MAX);
-	scanf_s("%d", &percentile_probability);
-	if (percentile_probability  < PROB_MIN || percentile_probability  > PROB_MAX) {
-		
-		
-
-	}
-	return 0;
-
+	intro();
+	train_length = variables("train length", LEN_MIN, LEN_MAX);
+	madongseok_stamina = variables("madongseok stamina", STM_MIN, STM_MAX);
+	percentile_probability = variables("percentile probability \'p\'", PROB_MIN, PROB_MAX);
+	first_train = train(zombie, citizen, madongseok);
 }
